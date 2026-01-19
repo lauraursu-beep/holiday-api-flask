325a39b256ef657afae4e0a383da5d27548bed63
laura ursu <laura.ursu@madetech.com>
2026-01-19 13:07:07 +0000

    Used json2html to convert json in html

---
+++ convert_json.py
@@ -0,0 +1,12 @@
+from json2html import json2html
+import json 
+
+def convert_json_table():
+    with open('file.json','r') as file:
+        data = json.load(file)
+    
+    table_html = json2html.convert(json = data)
+    with open('table.html','w') as file:
+        file.write(table_html)
+    
+    return("HTML table generated") 
