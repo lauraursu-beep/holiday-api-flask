cc107ff5de13763d8db6f10983f7c16c0123b85c
laura ursu <laura.ursu@madetech.com>
2026-01-19 13:05:09 +0000

    Add TDD to test convert json

---
+++ test_table.py
@@ -0,0 +1,12 @@
+from convert_json import convert_json_table
+import unittest
+
+class Convert_JSON_To_Table_TestCase(unittest.TestCase):
+    def test_return_message(self):
+        message = convert_json_table()
+        self.assertEqual(message, "HTML table generated")
+
+
+if __name__ == '__main__':
+    unittest.main()
+
