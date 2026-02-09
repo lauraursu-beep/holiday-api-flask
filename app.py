4e4983738fafb395b1bca78d3aa3d6c4d67b73ce
laura ursu <laura.ursu@madetech.com>
2026-02-09 01:25:19 +0000

    Added a function to save user input to json file

--- app.py
+++ app.py
@@ -1,30 +1,46 @@
-from flask import Flask, jsonify, render_template,request
+from flask import Flask, jsonify, render_template,request, redirect, url_for
 import json
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
+def home():
+    return render_template('HomePage.html')
+
+@app.get('/holidays')
 def create_table():
     with open('file.json', 'r')as file:
         holidays = json.load(file)
 
-    return render_template('table.html', data=holidays)
+    return render_template('TablePage.html', data=holidays)
 
-@app.route('/jsonify', methods=['GET'])
-def get_holiday():
-    with open('file.json', 'r') as f:
-        holidays = json.load(f)
-    return jsonify(holidays)  
+@app.get('/holidays/add')
+def add_holiday_page():
+    return render_template('AddHolidayPage.html')
 
-@app.route('/holiday', methods=['POST'])
-def create_holiday():
-    holidays = request.get_json()
-    return jsonify({
-        'message': 'Holiday created',
-        'holiday': holidays
-    }), 201
+@app.post('/holidays')
+def add_holiday_to_file():
+    holiday_name = request.form.get('holidayName')
+    holiday_start_date = request.form.get('startDate')
+    holiday_end_date = request.form.get('endDate')
 
+    new_holiday = {
+        'holidayName': holiday_name,
+        'startDate': holiday_start_date,
+        'endDate': holiday_end_date
+    }
 
+    with open('file.json','r')as file:
+        holidays = json.load(file)
+
+    holidays.append(new_holiday)
+
+    with open('file.json','w') as file:
+        json.dump(holidays,file,indent=4)
+    
+    return  redirect(url_for('create_table'))
+
+    
 if __name__ == '__main__':
     app.run(debug=True)
 
